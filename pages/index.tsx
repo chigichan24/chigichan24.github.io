77d8cfe471b1adea8b61236b6ce587b3276148f5
Kazuki Chigita <kazuki.chigichan@gmail.com>
2022-01-29 19:19:35 +0900

    Add navigation route for icons page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type {NextPage} from 'next'
 import styles from '../styles/Home.module.css'
 import MyHeader from "../components/MyHeader";
+import Link from 'next/link';
 
 const Home: NextPage = () => {
     return (
@@ -8,7 +9,13 @@ const Home: NextPage = () => {
             <MyHeader headerTitle="Home"/>
             <div className={styles.container}>
                 <div className={styles.content}>
-                    ゆっくりしていってね。
+                    ここは chigichan24 の個人ページです。
+                </div>
+                <div className={styles.content}>
+                    アイコン一覧は
+                    <Link href="/icons">
+                        <a>こちら</a>
+                    </Link>
                 </div>
             </div>
         </>
