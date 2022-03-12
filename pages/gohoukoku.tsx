420232248b5779c905533113777ea25af87772c9
Kazuki Chigita <kazuki.chigichan@gmail.com>
2022-03-12 19:24:28 +0900

    Special feature

---
+++ pages/gohoukoku.tsx
@@ -0,0 +1,26 @@
+import type {NextPage} from 'next'
+import styles from '../styles/gohoukoku.module.css'
+import MyHeader from "../components/MyHeader";
+
+const Home: NextPage = () => {
+    return (
+        <>
+            <MyHeader headerTitle="Home"/>
+            <div className={styles.container}>
+                <div className={styles.content}>
+                <ruby>
+                <rb>結婚</rb>
+                <rp>（</rp>
+                <rt>ケコーン</rt>
+                <rp>）</rp>
+                </ruby>
+                しました。
+                </div>
+                <img className={styles.imageContent } alt="chigichan24_2017"
+                                     src="/logo/github/chigichan24_2017.jpg" width={300} height={300}/>
+            </div>
+        </>
+    )
+}
+
+export default Home;
