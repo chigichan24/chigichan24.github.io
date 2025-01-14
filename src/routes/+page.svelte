2722050ab926bf86fe2fd644b15fbd4cbe48fc64
Kazuki Chigita <kazuki.chigichan@gmail.com>
2025-01-14 23:35:24 +0900

    Clear current codebasese

--- src/routes/+page.svelte
+++
@@ -1,3 +0,0 @@
-<h1>Kazuki Chigita (chigichan24)</h1>
-<p>Works as Android application developer</p>
-<p>For more detail, visit <a href="https://drive.google.com/file/d/1CT-qT_EpyoB4-0UH-LU8v6VYuNqfWN6j/view">my resume</a></p>
