4723b309972c7dd9c33c81fb91003e554d36bd43
Kazuki Chigita <kazuki.chigichan@gmail.com>
2025-02-24 21:13:41 +0900

    Update order

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,6 +13,20 @@ export function About() {
       </section>
 
       <section className="about-section">
+        <h3>{t("about.experience")}</h3>
+        <ul className="experience-list">
+          <li className="experience-item">
+            <div className="experience-title">{t("about.experience.current.company")}</div>
+            <div className="experience-detail">
+              <span className="position">{t("about.experience.current.position")}</span>
+              <span className="period">{t("about.experience.current.period")}</span>
+            </div>
+            <div className="experience-description">{t("about.experience.current.description")}</div>
+          </li>
+        </ul>
+      </section>
+
+      <section className="about-section">
         <h3>{t("about.education")}</h3>
         <ul className="education-list">
         <li className="education-item">
@@ -29,20 +43,6 @@ export function About() {
             <div className="education-title">{t("about.education.high.name")}</div>
             <div className="education-detail">{t("about.education.high.department")}</div>
             <div className="education-detail">{t("about.education.high.type")}</div>
-          </li>
-        </ul>
-      </section>
-
-      <section className="about-section">
-        <h3>{t("about.experience")}</h3>
-        <ul className="experience-list">
-          <li className="experience-item">
-            <div className="experience-title">{t("about.experience.current.company")}</div>
-            <div className="experience-detail">
-              <span className="position">{t("about.experience.current.position")}</span>
-              <span className="period">{t("about.experience.current.period")}</span>
-            </div>
-            <div className="experience-description">{t("about.experience.current.description")}</div>
           </li>
         </ul>
       </section>
