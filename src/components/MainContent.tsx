ca05d8df65f8560512b18eca9994bde6303e5850
Kazuki Chigita <kazuki.chigichan@gmail.com>
2025-02-24 21:39:50 +0900

    Update about page

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,11 +1,15 @@
 import { useLanguage } from "../contexts/LanguageContext";
+import "./MainContent.css";
 
 export function MainContent() {
   const { t } = useLanguage();
   
   return (
-    <main>
-      <p>{t("main.content")}</p>
+    <main className="main-content">
+      <div className="geometric-bg" />
+      <div className="content-wrapper">
+        <h1 className="title">{t("main.content")}</h1>
+      </div>
     </main>
   );
 } 
